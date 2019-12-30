c9e358fd7a6c3acdf1ab13caa8eeaa3b67386c11
Doodlis <58992405+Doodlis@users.noreply.github.com>
2019-12-30 15:50:20 -0600

    Shopping List

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,18 +1,7 @@
 function sample(){
 
-// Example
-var ourArray = ["Stimpson", "J", "cat"];
-ourArray.shift(); // ourArray now equals ["J", "cat"]
-ourArray.unshift("Happy");
-// ourArray now equals ["Happy", "J", "cat"]
-
-// Setup
-var myArray = [["John", 23], ["dog", 3]];
-myArray.shift();
+    var myList = [["Butter", 2], ["Milk", 1], ["Coffee", 4], ["Bread", 7], ["Salmon", 15]];
 
 
-// Only change code below this line.
-myArray.unshift(["Paul",35]);
-
-    return myArray;
+    return myList;
 }
