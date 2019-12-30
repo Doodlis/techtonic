9708d4d541fb92811a2f64a499078c185550c6e0
Doodlis <58992405+Doodlis@users.noreply.github.com>
2019-12-30 15:17:51 -0600

    Store Multiple Values in one Variable using JavaScript Arrays

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,25 +1,12 @@
 function sample(){
 
 
-
-    // Example
-var firstNameLength = 0;
-var firstName = "Ada";
-
 // Example
-var firstLetterOfFirstName = "";
-var firstName = "Ada";
+var ourArray = ["John", 23];
 
-firstLetterOfFirstName = firstName[0];
-
-var myNoun = "dog";
-var myAdjective = "big";
-var myVerb = "ran";
-var myAdverb = "quickly";
-
-var wordBlanks = "When I was little I had a " + myNoun + ". Her name was Nika and she was very " + myAdjective + " and fluffy. Nika " + myVerb + " after all the  children in the neighborghood whenerver she had a chance. Kids had to sprint really " + myAdverb + " to escape her loving face-licks."; // Only change this line;
+// Only change code below this line.
+var myArray = ["Doodlis", 28];
 
 
-
-    return wordBlanks;
+    return myArray;
 }
