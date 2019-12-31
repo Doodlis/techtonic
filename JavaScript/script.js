a854d157a1bbc52004bbba081a51c5f6cac49fcf
Doodlis <58992405+Doodlis@users.noreply.github.com>
2019-12-31 15:16:19 -0600

    Iterate Odd Numbers With a For Loop

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -864,5 +864,22 @@ function iterateWithJavaScriptForLoops() {
         myArray.push(i);
     }
     // Only change code below this line.
-return myArray;
+    return myArray;
+}
+
+function iterateOddNumbersWithAForLoop() {
+    // Example
+    var ourArray = [];
+
+    for (var i = 0; i < 10; i += 2) {
+        ourArray.push(i);
+    }
+
+    // Setup
+    var myArray = [];
+    for (var i = 1; i <= 9; i += 2) {
+        myArray.push(i)
+    }
+    // Only change code below this line.
+
 }
