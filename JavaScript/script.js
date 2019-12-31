ca75ea62fc54e13214c5e43813c5327e46168762
Doodlis <58992405+Doodlis@users.noreply.github.com>
2019-12-31 12:27:56 -0600

    Using Objects for Lookups

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -649,3 +649,25 @@ function deletePropertiesFromAJavaScriptObject() {
     // Only change code below this line.
     return myDog.tails;
 }
+
+function usingObjectsForLookups() {// Setup
+    function phoneticLookup(val) {
+        var result = "";
+
+        // Only change code below this line
+        var lookup = {
+            "alpha": "Adams",
+            "bravo": "Boston",
+            "charlie": "Chicago",
+            "delta": "Denver",
+            "echo": "Easy",
+            "foxtrot": "Frank"
+        }
+        result = lookup[val];
+        // Only change code above this line
+        return result;
+    }
+
+    // Change this value to test
+    return phoneticLookup("charlie");
+}
