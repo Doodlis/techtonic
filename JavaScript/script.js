9398ccd622d729aa2056455b07f5927978a8542d
Doodlis <58992405+Doodlis@users.noreply.github.com>
2019-12-31 10:22:25 -0600

    Adding a Default Option in Switch Statements

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -349,7 +349,7 @@ function selectingFromManyOptionsWithSwitchStatements() {
                 break;
         }
 
-    
+
 
         // Only change code above this line
         return answer;
@@ -357,4 +357,32 @@ function selectingFromManyOptionsWithSwitchStatements() {
 
     // Change this value to test
     return caseInSwitch(4);
+}
+function addingADefaultOptionInSwitchStatements() {
+    function switchOfStuff(val) {
+        var answer = "";
+        // Only change code below this line
+
+        switch (val) {
+            case "a":
+                answer = "apple";
+                break;
+            case "b":
+                answer = "bird";
+                break;
+            case "c":
+                answer = "cat";
+                break;
+            default:
+                answer = "stuff";
+                break;
+
+        }
+        // Only change code above this line
+        return answer;
+    }
+
+    // Change this value to test
+    return switchOfStuff("c");
+
 }
