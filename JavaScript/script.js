490ea52c0a6c0631a6359cf95c6264bf29a4e8a4
Doodlis <58992405+Doodlis@users.noreply.github.com>
2019-12-31 11:36:14 -0600

    Accessing Object Properties with Bracket Notation

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -543,3 +543,17 @@ function accessingObjectPropertiesWithDotNotation() {
     var shirtValue = testObj.shirt;    // Change this line
 return [hatValue, shirtValue];
 }
+function accessingObjectPropertiesWithBracketNotation() {
+    // Setup
+var testObj = {
+    "an entree": "hamburger",
+    "my side": "veggies",
+    "the drink": "water"
+  };
+  
+  // Only change code below this line
+  
+  var entreeValue = testObj["an entree"];   // Change this line
+  var drinkValue = testObj["the drink"];    // Change this line
+  return [entreeValue, drinkValue];
+}
