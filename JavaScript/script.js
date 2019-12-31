2660dc136ebb5b00af3eb4bc7bcffc362801f5c4
Doodlis <58992405+Doodlis@users.noreply.github.com>
2019-12-31 10:51:08 -0600

    Return Early Pattern for Functions

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -391,69 +391,87 @@ function multipleIdenticaOptionInSwitchStatements() {
     function sequentialSizes(val) {
         var answer = "";
         // Only change code below this line
-      
-      switch(val){
-        case 1:
-        case 2:
-        case 3:
-          answer = "Low";
-          break;
-        case 4:
-        case 5:
-        case 6:
-          answer ="Mid";
-          break;
-        case 7:
-        case 8:
-        case 9:
-          answer = "High";
-      
-      }
-      
-      
+
+        switch (val) {
+            case 1:
+            case 2:
+            case 3:
+                answer = "Low";
+                break;
+            case 4:
+            case 5:
+            case 6:
+                answer = "Mid";
+                break;
+            case 7:
+            case 8:
+            case 9:
+                answer = "High";
+
+        }
+
+
         // Only change code above this line
         return answer;
-      }
-      
-      // Change this value to test
-      return sequentialSizes(9);
+    }
+
+    // Change this value to test
+    return sequentialSizes(9);
 }
 
-function  replacingIfElseChainsWithSwitch() {
+function replacingIfElseChainsWithSwitch() {
     function chainToSwitch(val) {
         var answer = "";
         // Only change code below this line
-      switch(val){
-        case "bob":
-          answer = "Marley";
-          break;
-        case 42: 
-          answer = "The Answer";
-          break;
-        case 1: 
-          answer = "There is no #1";
-          break;
-        case 99: 
-          answer = "Missed me by this much!";
-          break;
-        case 7:
-          answer = "Ate Nine";
-          break;
+        switch (val) {
+            case "bob":
+                answer = "Marley";
+                break;
+            case 42:
+                answer = "The Answer";
+                break;
+            case 1:
+                answer = "There is no #1";
+                break;
+            case 99:
+                answer = "Missed me by this much!";
+                break;
+            case 7:
+                answer = "Ate Nine";
+                break;
         }
-      
+
         // Only change code above this line
         return answer;
-      }
-      
-      // Change this value to test
-      return chainToSwitch(7);
-}  
+    }
+
+    // Change this value to test
+    return chainToSwitch(7);
+}
 function returningBooleanValuesFromFunctions() {
     function isLess(a, b) {
         // Fix this code
-        return a < b; 
-      }
-      
-      // Change these values to test
-      return isLess(15, 10);
+        return a < b;
+    }
+
+    // Change these values to test
+    return isLess(15, 10);
+}
+
+function returnEarlyPatternForFunctions() {
+    // Setup
+    function abTest(a, b) {
+        // Only change code below this line
+        if (a < 0 || b < 0) {
+            return undefined;
+        }
+
+
+        // Only change code above this line
+
+        return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+    }
+
+    // Change values below to test your code
+    return abTest(2, 2);
 }
