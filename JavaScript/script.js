af140a7024368ad9e71ec01c54ff614a83cce7cb
Doodlis <58992405+Doodlis@users.noreply.github.com>
2019-12-30 23:29:31 -0600

    Logical Order in If Else Statements

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -263,3 +263,17 @@ function introducingElseIfStatements() {
     return testElseIf(10);
 
 }
+function logicalOrderInIfElseStatements() {
+    function orderMyLogic(val) {
+        if (val < 5) {
+          return "Less than 5";
+        } else if (val < 10) {
+          return "Less than 10";
+        } else {
+          return "Greater than or equal to 10";
+        }
+      }
+      
+      // Change this value to test
+      return orderMyLogic(7);
+}
