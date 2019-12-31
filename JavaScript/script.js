e026d029c93bb90fda98000d28f0124ec7963f9f
Doodlis <58992405+Doodlis@users.noreply.github.com>
2019-12-30 21:27:24 -0600

    Comparison with the Equality Operator

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,62 +1,73 @@
-function nextInLineAssignment(){
+function nextInLineAssignment() {
 
     function nextInLine(arr, item) {
         arr.push(item);
 
-        return arr.shift(); 
-      }
-      
-      // Test Setup
-      var testArr = [1,2,3,4,5];
-      
-      // Display Code
-      console.log("Before: " + JSON.stringify(testArr));
-      console.log(nextInLine(testArr, 6)); // Modify this line to test
-      console.log("After: " + JSON.stringify(testArr));
+        return arr.shift();
+    }
+
+    // Test Setup
+    var testArr = [1, 2, 3, 4, 5];
+
+    // Display Code
+    console.log("Before: " + JSON.stringify(testArr));
+    console.log(nextInLine(testArr, 6)); // Modify this line to test
+    console.log("After: " + JSON.stringify(testArr));
 }
 
-function understandingBooleanValues(){
+function understandingBooleanValues() {
 
     function welcomeToBooleans() {
 
         // Only change code below this line.
-      
-        return true; // Change this line
-      
-        // Only change code above this line.
-      }   
 
-      console.log(welcomeToBooleans());
+        return true; // Change this line
+
+        // Only change code above this line.
+    }
+
+    console.log(welcomeToBooleans());
 }
 
-function useConditionalLogicwithIfStatements(){
+function useConditionalLogicwithIfStatements() {
     // Example
     function ourTrueOrFalse(isItTrue) {
-        if (isItTrue) {
-        return "Yes, it's true";
-        }
+        if (isItTrue) return "Yes, it's true";
+
         return "No, it's false";
     }
-    
-    
+
     // Setup
     function trueOrFalse(wasThatTrue) {
-    
+
         // Only change code below this line.
-    if(wasThatTrue) {
-        return "Yes, that was true";
-    }
-    else{
-        return "No, that was false";
-    }
+        if (wasThatTrue) {
+            return "Yes, that was true";
+        }
+        else {
+            return "No, that was false";
+        }
         // Only change code above this line.
-    
+
     }
-    
+
     // Change this value to test
     trueOrFalse("true");
-    
 }
 
+function comparisonWithTheEqualityOperator() {
+
+    // Setup
+    function testEqual(val) {
+        if (val == 12) { // Change this line
+            return "Equal";
+        }
+        return "Not Equal";
+    }
+
+    // Change this value to test
+    testEqual(10);
+
+}
 
 
