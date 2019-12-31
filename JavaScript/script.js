c1021d71f1a101c910f86b9c6e02e45f8bc65e84
Doodlis <58992405+Doodlis@users.noreply.github.com>
2019-12-30 21:10:30 -0600

    Use Conditional Logic with If Statements

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -16,7 +16,7 @@ function nextInLineAssignment(){
 }
 
 function understandingBooleanValues(){
-    
+
     function welcomeToBooleans() {
 
         // Only change code below this line.
@@ -29,7 +29,34 @@ function understandingBooleanValues(){
       console.log(welcomeToBooleans());
 }
 
+function useConditionalLogicwithIfStatements(){
+    // Example
+    function ourTrueOrFalse(isItTrue) {
+        if (isItTrue) {
+        return "Yes, it's true";
+        }
+        return "No, it's false";
+    }
+    
+    
+    // Setup
+    function trueOrFalse(wasThatTrue) {
+    
+        // Only change code below this line.
+    if(wasThatTrue) {
+        return "Yes, that was true";
+    }
+    else{
+        return "No, that was false";
+    }
+        // Only change code above this line.
+    
+    }
+    
+    // Change this value to test
+    trueOrFalse("true");
+    
+}
 
 
- 
 
