31befdccc361a986f0ea31f863fab0037bb06923
Doodlis <58992405+Doodlis@users.noreply.github.com>
2019-12-30 21:54:10 -0600

    Comparison with the Strict Equality Operator

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -70,4 +70,18 @@ function comparisonWithTheEqualityOperator() {
 
 }
 
+function comparisonWithTheStrictEqualityOperator() {
+    // Setup
+function testStrict(val) {
+    if (val === 7) { // Change this line
+      return "Equal";
+    }
+    return "Not Equal";
+  }
+  
+  // Change this value to test
+  testStrict("7");
+  
+}
+
 
