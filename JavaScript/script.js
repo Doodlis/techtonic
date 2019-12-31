bc841ccb45d0bd3efcf4838be1a64d707d13a3e2
Doodlis <58992405+Doodlis@users.noreply.github.com>
2019-12-31 12:43:21 -0600

    Testing Objects for Properties

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -669,5 +669,26 @@ function usingObjectsForLookups() {// Setup
     }
 
     // Change this value to test
-    return phoneticLookup("charlie");
+    return phoneticLookup("echo");
+}
+
+function testingObjectsForProperties() {
+    // Setup
+    var myObj = {
+        gift: "pony",
+        pet: "kitten",
+        bed: "sleigh"
+    };
+
+    function checkObj(checkProp) {
+        // Your Code Here
+        if (myObj.hasOwnProperty(checkProp) == true) {
+            return myObj[checkProp];
+        }
+        else {
+            return "Not Found"
+        }
+    }
+    // Test your code by modifying these values
+    return checkObj("gift");
 }
