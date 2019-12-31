0929fe81940f0fdbdc69332a20a0748435265f87
Doodlis <58992405+Doodlis@users.noreply.github.com>
2019-12-31 11:53:10 -0600

    Add New Properties to a JavaScript Object

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -603,3 +603,24 @@ function updatingObjectProperties(){
       return myDog.name;
 }
 
+function addNewPropertiesToAJavaScriptObject(){
+    var ourDog = {
+        "name": "Camper",
+        "legs": 4,
+        "tails": 1,
+        "friends": ["everything!"]
+      };
+      
+      ourDog.bark = "bow-wow";
+      
+      // Setup
+      var myDog = {
+        "name": "Happy Coder",
+        "legs": 4,
+        "tails": 1,
+        "friends": ["freeCodeCamp Campers"]
+      };
+      myDog.bark = "woof";
+      // Only change code below this line.
+      return myDog.bark;
+}
