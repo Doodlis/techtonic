3d3ce0a17eada02e87d3f4ef555542e7aebffb19
Doodlis <58992405+Doodlis@users.noreply.github.com>
2019-12-31 11:55:41 -0600

    Delete Properties from a JavaScript Object

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -581,46 +581,71 @@ function AccessingObjectPropertiesWithVariables() {
     return player;
 }
 
-function updatingObjectProperties(){
+function updatingObjectProperties() {
     var ourDog = {
         "name": "Camper",
         "legs": 4,
         "tails": 1,
         "friends": ["everything!"]
-      };
-      
-      ourDog.name = "Happy Camper";
-      
-      // Setup
-      var myDog = {
+    };
+
+    ourDog.name = "Happy Camper";
+
+    // Setup
+    var myDog = {
         "name": "Coder",
         "legs": 4,
         "tails": 1,
         "friends": ["freeCodeCamp Campers"]
-      };
-      myDog.name = "Happy Coder"; 
-      // Only change code below this line.
-      return myDog.name;
+    };
+    myDog.name = "Happy Coder";
+    // Only change code below this line.
+    return myDog.name;
 }
 
-function addNewPropertiesToAJavaScriptObject(){
+function addNewPropertiesToAJavaScriptObject() {
     var ourDog = {
         "name": "Camper",
         "legs": 4,
         "tails": 1,
         "friends": ["everything!"]
-      };
-      
-      ourDog.bark = "bow-wow";
-      
-      // Setup
-      var myDog = {
+    };
+
+    ourDog.bark = "bow-wow";
+
+    // Setup
+    var myDog = {
         "name": "Happy Coder",
         "legs": 4,
         "tails": 1,
         "friends": ["freeCodeCamp Campers"]
-      };
-      myDog.bark = "woof";
-      // Only change code below this line.
-      return myDog.bark;
+    };
+    myDog.bark = "woof";
+    // Only change code below this line.
+    return myDog.bark;
+}
+
+function deletePropertiesFromAJavaScriptObject() {
+    // Example
+    var ourDog = {
+        "name": "Camper",
+        "legs": 4,
+        "tails": 1,
+        "friends": ["everything!"],
+        "bark": "bow-wow"
+    };
+
+    delete ourDog.bark;
+
+    // Setup
+    var myDog = {
+        "name": "Happy Coder",
+        "legs": 4,
+        "tails": 1,
+        "friends": ["freeCodeCamp Campers"],
+        "bark": "woof"
+    };
+    delete myDog.tails;
+    // Only change code below this line.
+    return myDog.tails;
 }
