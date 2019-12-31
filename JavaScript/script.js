3fe3b64b8b5ab0ca0b529f61bf826002167b0268
Doodlis <58992405+Doodlis@users.noreply.github.com>
2019-12-30 22:18:22 -0600

    Comparison with the Less Than Operator

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -156,3 +156,20 @@ function comparisonWithTheGreaterThanOrEqualToOperator() {
       // Change this value to test
       testGreaterOrEqual(10);
 }
+
+function comparisonWithTheLessThanOperator() {
+    function testLessThan(val) {
+        if (val < 25) {  // Change this line
+          return "Under 25";
+        }
+      
+        if (val < 55) {  // Change this line
+          return "Under 55";
+        }
+      
+        return "55 or Over";
+      }
+      
+      // Change this value to test
+      testLessThan(28);
+}
