777db01a50d8679864797d086dd215926baf63c1
Doodlis <58992405+Doodlis@users.noreply.github.com>
2019-12-31 15:09:15 -0600

    Iterate with JavaScript For Loops

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -830,7 +830,7 @@ function recordCollection() {
 
     // Alter values below to test your code
     updateRecords(5439, "artist", "ABBA");
-    
+
 
 }
 
@@ -848,3 +848,21 @@ function iteratewithJavaScriptWhileLoops() {
     // Only change code below this line.
 }
 
+function iterateWithJavaScriptForLoops() {
+    // Example
+    var ourArray = [];
+
+    for (var i = 0; i < 5; i++) {
+        ourArray.push(i);
+    }
+
+
+    // Setup
+    var myArray = [];
+
+    for (var i = 1; i <= 5; i++) {
+        myArray.push(i);
+    }
+    // Only change code below this line.
+return myArray;
+}
