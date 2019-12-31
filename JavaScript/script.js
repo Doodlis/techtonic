ad6fc6289fb90f9e5def4d77ba36e7f79bda1e87
Doodlis <58992405+Doodlis@users.noreply.github.com>
2019-12-30 23:23:16 -0600

    Introducing Else If Statements

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -126,121 +126,140 @@ function comparisonWithTheStrictInequalityOperator() {
 function comparisionWithGreaterThanOperator() {
     function testGreaterThan(val) {
         if (val > 100) {  // Change this line
-          return "Over 100";
+            return "Over 100";
         }
-      
+
         if (val > 10) {  // Change this line
-          return "Over 10";
+            return "Over 10";
         }
-      
+
         return "10 or Under";
-      }
-      
-      // Change this value to test
-      return testGreaterThan(3);
+    }
+
+    // Change this value to test
+    return testGreaterThan(3);
 }
 
 function comparisonWithTheGreaterThanOrEqualToOperator() {
     function testGreaterOrEqual(val) {
         if (val >= 20) {  // Change this line
-          return "20 or Over";
+            return "20 or Over";
         }
-      
-        if (val >=10) {  // Change this line
-          return "10 or Over";
+
+        if (val >= 10) {  // Change this line
+            return "10 or Over";
         }
-      
+
         return "Less than 10";
-      }
-      
-      // Change this value to test
-      return testGreaterOrEqual(10);
+    }
+
+    // Change this value to test
+    return testGreaterOrEqual(10);
 }
 
 function comparisonWithTheLessThanOperator() {
     function testLessThan(val) {
         if (val < 25) {  // Change this line
-          return "Under 25";
+            return "Under 25";
         }
-      
+
         if (val < 55) {  // Change this line
-          return "Under 55";
+            return "Under 55";
         }
-      
+
         return "55 or Over";
-      }
-      
-      // Change this value to test
-      return testLessThan(28);
+    }
+
+    // Change this value to test
+    return testLessThan(28);
 }
 
 function comparisonWithTheLessThanOrEqualToOperator() {
     function testLessOrEqual(val) {
         if (val <= 12) {  // Change this line
-          return "Smaller Than or Equal to 12";
+            return "Smaller Than or Equal to 12";
         }
-      
+
         if (val <= 24) {  // Change this line
-          return "Smaller Than or Equal to 24";
+            return "Smaller Than or Equal to 24";
         }
-      
+
         return "More Than 24";
-      }
-      
-      // Change this value to test
-      return testLessOrEqual(10);
-      
+    }
+
+    // Change this value to test
+    return testLessOrEqual(10);
+
 }
 
 function comparisonsWithTheLogicalAndOperator() {
     function testLogicalAnd(val) {
         // Only change code below this line
-      
+
         if (val <= 50 && val >= 25) {
             return "Yes";
-          }
-      
+        }
+
         // Only change code above this line
         return "No";
-      }
-      
-      // Change this value to test
-      return testLogicalAnd(25);
-      
+    }
+
+    // Change this value to test
+    return testLogicalAnd(25);
+
 }
 
 function comparisonsWithTheLogicalOrOperator() {
     function testLogicalOr(val) {
         // Only change code below this line
-      
+
         if (val < 10 || val > 20) {
-          return "Outside";
+            return "Outside";
         }
-      
-      
+
+
         // Only change code above this line
         return "Inside";
-      }
-      
-      // Change this value to test
-      return testLogicalOr(25);
+    }
+
+    // Change this value to test
+    return testLogicalOr(25);
 }
 function introducingElseStatements() {
     function testElse(val) {
-    var result = "";
-    // Only change code below this line
-  
-    if (val > 5) {
-      result = "Bigger than 5";
+        var result = "";
+        // Only change code below this line
+
+        if (val > 5) {
+            result = "Bigger than 5";
+        }
+
+        else {
+            result = "5 or Smaller";
+        }
+
+        // Only change code above this line
+        return result;
     }
-  
-    else {
-      result = "5 or Smaller";
+
+    // Change this value to test
+    return testElse(10);
+}
+
+function introducingElseIfStatements() {
+    function testElseIf(val) {
+        if (val > 10) {
+            return "Greater than 10";
+        }
+
+        else if (val < 5) {
+            return "Smaller than 5";
+        }
+        else {
+            return "Between 5 and 10";
+        }
     }
-  
-    // Only change code above this line
-    return result;
-  }
-  
-  // Change this value to test
-  return testElse(10);}
+    // Change this value to test
+    return testElseIf(10);
+
+}
