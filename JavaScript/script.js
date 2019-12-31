7422df9ec6d824f4f351b90e4f96e738b1d8ad42
Doodlis <58992405+Doodlis@users.noreply.github.com>
2019-12-30 22:16:25 -0600

    Comparison with the Greater Than Or Equal To Operator

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -139,3 +139,20 @@ function comparisionWithGreaterThanOperator() {
       // Change this value to test
       testGreaterThan(3);
 }
+
+function comparisonWithTheGreaterThanOrEqualToOperator() {
+    function testGreaterOrEqual(val) {
+        if (val >= 20) {  // Change this line
+          return "20 or Over";
+        }
+      
+        if (val >=10) {  // Change this line
+          return "10 or Over";
+        }
+      
+        return "Less than 10";
+      }
+      
+      // Change this value to test
+      testGreaterOrEqual(10);
+}
