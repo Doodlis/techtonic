68ba88b76966c2962856925d56da804aea3c386f
Doodlis <58992405+Doodlis@users.noreply.github.com>
2019-12-31 17:40:06 -0600

    Generate Random Whole Numbers with JavaScript

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1025,3 +1025,15 @@ function GenerateRandomFractionsWithJavaScript() {
       }
       
 }
+
+function generateRandomWholeNumbersWithJavaScript() {
+    var randomNumberBetween0and19 = Math.floor(Math.random() * 20);
+
+function randomWholeNum() {
+
+  // Only change code below this line.
+
+  return Math.floor(Math.random() * 10);
+}
+
+}
