5d5c028a7bb754818861bfffb5d01a85aab3a8c3
Doodlis <58992405+Doodlis@users.noreply.github.com>
2019-12-30 23:17:31 -0600

    Introducing Else Statements

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -225,3 +225,22 @@ function comparisonsWithTheLogicalOrOperator() {
       // Change this value to test
       return testLogicalOr(25);
 }
+function introducingElseStatements() {
+    function testElse(val) {
+    var result = "";
+    // Only change code below this line
+  
+    if (val > 5) {
+      result = "Bigger than 5";
+    }
+  
+    else {
+      result = "5 or Smaller";
+    }
+  
+    // Only change code above this line
+    return result;
+  }
+  
+  // Change this value to test
+  return testElse(10);}
