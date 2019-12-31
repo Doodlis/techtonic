28e1eeae51ba2aedcfd77fbfb9d08652867eda68
Doodlis <58992405+Doodlis@users.noreply.github.com>
2019-12-31 11:14:59 -0600

    Counting Cards

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -475,3 +475,38 @@ function returnEarlyPatternForFunctions() {
     // Change values below to test your code
     return abTest(2, 2);
 }
+
+function countingCards() {
+    var count = 0;
+
+function cc(card) {
+  // Only change code below this line  
+ switch (card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count++;
+      break;
+    case 10:
+    case "J":
+    case "Q":
+    case "K":
+    case "A":
+      count--;
+      break;
+  }
+  if (count > 0) {
+    return count + " Bet";
+  } else {
+    return count + " Hold";
+  }
+
+  // Only change code above this line
+}
+
+// Add/remove calls to test your function.
+// Note: Only the last will display
+return cc("A"), cc(2), cc(6), cc(10), cc(5); 
+}
