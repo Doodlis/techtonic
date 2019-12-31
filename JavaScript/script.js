3ba66b0ae4ee731dfb671f9a425de10f89c9d0ec
Doodlis <58992405+Doodlis@users.noreply.github.com>
2019-12-31 11:15:26 -0600

    format

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -479,34 +479,34 @@ function returnEarlyPatternForFunctions() {
 function countingCards() {
     var count = 0;
 
-function cc(card) {
-  // Only change code below this line  
- switch (card) {
-    case 2:
-    case 3:
-    case 4:
-    case 5:
-    case 6:
-      count++;
-      break;
-    case 10:
-    case "J":
-    case "Q":
-    case "K":
-    case "A":
-      count--;
-      break;
-  }
-  if (count > 0) {
-    return count + " Bet";
-  } else {
-    return count + " Hold";
-  }
+    function cc(card) {
+        // Only change code below this line  
+        switch (card) {
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+                count++;
+                break;
+            case 10:
+            case "J":
+            case "Q":
+            case "K":
+            case "A":
+                count--;
+                break;
+        }
+        if (count > 0) {
+            return count + " Bet";
+        } else {
+            return count + " Hold";
+        }
 
-  // Only change code above this line
-}
+        // Only change code above this line
+    }
 
-// Add/remove calls to test your function.
-// Note: Only the last will display
-return cc("A"), cc(2), cc(6), cc(10), cc(5); 
+    // Add/remove calls to test your function.
+    // Note: Only the last will display
+    return cc("A"), cc(2), cc(6), cc(10), cc(5);
 }
