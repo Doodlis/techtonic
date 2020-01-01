895120a4684d4f1cb5e951cf655f04c463b1c31a
Doodlis <58992405+Doodlis@users.noreply.github.com>
2020-01-01 17:50:03 -0600

    Complete a Promise with resolve and reject

--- ES6 Challenges/index.js
+++ ES6 Challenges/index.js
@@ -51,3 +51,19 @@ function createAJavaScriptPromise() {
 
     });
 }
+
+function completeAPromiseWithResolveAndReject() {
+    const makeServerRequest = new Promise((resolve, reject) => {
+        // responseFromServer represents a response from a server
+        let responseFromServer;
+          
+        if(responseFromServer) {
+          resolve("We got the data");
+          // change this line
+        } else {  
+          reject("Data not received");
+          // change this line
+        }
+      });
+      
+}
