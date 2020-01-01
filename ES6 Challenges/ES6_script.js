eee7014b26118901bac2fb1d7833bd2fe6fa6540
Doodlis <58992405+Doodlis@users.noreply.github.com>
2020-01-01 15:52:56 -0600

    Write Concise Declarative Functions with ES6

--- ES6 Challenges/ES6_script.js
+++ ES6 Challenges/ES6_script.js
@@ -275,3 +275,17 @@ function writeConciseObjectLiteralDeclarationsUsingObjectPropertyShorthand() {
     console.log(createPerson("Zodiac Hasbro", 56, "male")); // returns a proper object
 
 }
+
+function writeConciseDeclarativeFunctionsWithES6() {
+    // change code below this line
+    const bicycle = {
+        gear: 2,
+        setGear(newGear) {
+            this.gear = newGear;
+        }
+    };
+    // change code above this line
+    bicycle.setGear(3);
+    console.log(bicycle.gear);
+
+}
