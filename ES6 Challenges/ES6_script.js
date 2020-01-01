f1e89e6128e21c1878b8dac2dfaa45dfc14106bd
Doodlis <58992405+Doodlis@users.noreply.github.com>
2019-12-31 19:42:00 -0600

    Declare a Read-Only Variable with the const Keyword

--- ES6 Challenges/ES6_script.js
+++ ES6 Challenges/ES6_script.js
@@ -33,5 +33,23 @@ function compareScopesOfTheVarAndLetKeywords() {
         console.log('Function scope i is: ', i);
         return i;
     }
-    return checkScope(i);
+return checkScope();
+}
+
+function declareAReadOnlyVariableWithTheConstKeyword() {
+    function printManyTimes(str) {
+        "use strict";
+
+        // change code below this line
+
+        const SENTENCE = str + " is cool!";
+        for (let i = 0; i < str.length; i += 2) {
+            console.log(SENTENCE);
+        }
+
+        // change code above this line
+
+    }
+     return printManyTimes("freeCodeCamp");
+
 }
