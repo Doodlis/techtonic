fa1fd2193f618b16e67143b6bbaed94b437d69fc
Doodlis <58992405+Doodlis@users.noreply.github.com>
2020-01-01 16:07:51 -0600

    Use class Syntax to Define a Constructor Function

--- ES6 Challenges/ES6_script.js
+++ ES6 Challenges/ES6_script.js
@@ -289,3 +289,17 @@ function writeConciseDeclarativeFunctionsWithES6() {
     console.log(bicycle.gear);
 
 }
+
+function useClassSyntaxToDefineAConstructorFunction() {
+    /* Alter code below this line */
+class Vegetable {
+    constructor(name){
+        this.name = name;
+    }
+}
+/* Alter code above this line */
+
+const carrot = new Vegetable('carrot');
+console.log(carrot.name); // => should be 'carrot'
+
+}
