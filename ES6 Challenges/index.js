39f95b00ead10c4773c85789ea7da2851b428526
Doodlis <58992405+Doodlis@users.noreply.github.com>
2020-01-01 18:01:22 -0600

    Handle a Rejected Promise with catch

--- ES6 Challenges/index.js
+++ ES6 Challenges/index.js
@@ -1,32 +1,32 @@
-function useExportToSharACodeBlock(){
+function useExportToSharACodeBlock() {
     const uppercaseString = (string) => {
-    return string.toUpperCase();
-  }
-  
-  const lowercaseString = (string) => {
-    return string.toLowerCase()
-  }
-  
-  export {uppercaseString, lowercaseString};
+        return string.toUpperCase();
+    }
+
+    const lowercaseString = (string) => {
+        return string.toLowerCase()
+    }
+
+    export { uppercaseString, lowercaseString };
 }
 
 
 function ReuseJavascriptCodeUsingImport() {
-    import { uppercaseString, lowercaseString} from './string_functions.js'
-  
-// add code above this line
+    import { uppercaseString, lowercaseString } from './string_functions.js'
 
-uppercaseString("hello");
-lowercaseString("WORLD!");
+    // add code above this line
+
+    uppercaseString("hello");
+    lowercaseString("WORLD!");
 
 }
 
 function useToImportEverythingFromAFile() {
     import * as stringFunctions from "./string_functions.js"
-// add code above this line
+    // add code above this line
 
-stringFunctions.uppercaseString("hello");
-stringFunctions.lowercaseString("WORLD!");
+    stringFunctions.uppercaseString("hello");
+    stringFunctions.lowercaseString("WORLD!");
 
 }
 
@@ -34,16 +34,16 @@ stringFunctions.lowercaseString("WORLD!");
 function exportADefaultExport() {
     export default function subtract(x, y) {
         return x - y;
-      }
-      
+    }
+
 }
 
 function importADefaultExport() {
     import subtract from "./math_functions.js"
     // add code above this line
-    
-    subtract(7,4);
-    
+
+    subtract(7, 4);
+
 }
 
 function createAJavaScriptPromise() {
@@ -56,30 +56,50 @@ function completeAPromiseWithResolveAndReject() {
     const makeServerRequest = new Promise((resolve, reject) => {
         // responseFromServer represents a response from a server
         let responseFromServer;
-          
-        if(responseFromServer) {
-          resolve("We got the data");
-          // change this line
-        } else {  
-          reject("Data not received");
-          // change this line
+
+        if (responseFromServer) {
+            resolve("We got the data");
+            // change this line
+        } else {
+            reject("Data not received");
+            // change this line
         }
-      });
-      
+    });
+
 }
 
 function handleAFulfilledPromiseWithThen() {
     const makeServerRequest = new Promise((resolve, reject) => {
         // responseFromServer is set to true to represent a successful response from a server
         let responseFromServer = true;
-          
-        if(responseFromServer) {
-          resolve("We got the data");
-        } else {  
-          reject("Data not received");
+
+        if (responseFromServer) {
+            resolve("We got the data");
+        } else {
+            reject("Data not received");
         }
-      
-        makeServerRequest.then(result => {console.log(result)});
-      });
-      
+
+        makeServerRequest.then(result => { console.log(result) });
+    });
+
+}
+
+function handleARejectedPromiseWithCatch() {
+    const makeServerRequest = new Promise((resolve, reject) => {
+        // responseFromServer is set to false to represent an unsuccessful response from a server
+        let responseFromServer = false;
+
+        if (responseFromServer) {
+            resolve("We got the data");
+        } else {
+            reject("Data not received");
+        }
+    });
+
+    makeServerRequest.then(result => {
+        console.log(result);
+    });
+    makeServerRequest.catch(error => {
+        console.log(error);
+    });
 }
